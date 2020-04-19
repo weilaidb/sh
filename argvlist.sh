460b0c02ae083a7322f31f6d203b1cbb9f9d4495
duo <duo@localhost.localdomain>
2020-04-19 16:21:55 +0800

    show argv list

---
+++ argvlist.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "====show argv list"
+
+showargvlist()
+{
+echo "参数个数为: $#"
+x=1
+until [ $# -eq 0 ]
+do
+printf "第%3d个参数为: $1\n"  $x
+shift
+((x=x+1))
+done
+
+}
+
+showargvlist $*
+
+
+
