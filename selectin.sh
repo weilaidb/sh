4b79a71ddb6f4aba53b77bca4e60eab4497a4f17
Your Name <you@example.com>
2020-04-25 11:04:31 +0800

    selectin change

--- selectin.sh
+++ selectin.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 #function:
+##?用来提示用户输入菜单编号；^D表示按下 Ctrl+D 组合键，它的作用是结束 select in 循环。
+
 
 echo "What is your favourite OS?"
 select name in "Linux" "Windows" "Mac OS" "UNIX" "Android" "WeilaiOS"
 do
     echo $name
-	echo "You have selected $name"
+	# echo "You have selected $name"
 done
+echo "You have selected $name"
 echo "Done"
-
 
 
 
