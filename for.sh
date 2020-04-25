21a7f60ebeb47adb0afa8aaf152980d7dcb1a800
Your Name <you@example.com>
2020-04-25 10:49:30 +0800

    for change

--- for.sh
+++ for.sh
@@ -10,4 +10,49 @@ done
 echo "The sum is: $sum"
 
 
+sum=0
+i=1
+for ((; i<=100; i++))
+do
+    ((sum += i))
+done
+echo "The sum is: $sum"
+
+
+
+
+
+sum=0
+for ((i=1; ; i++))
+do
+    if(( i>100 )); then
+        break
+    fi
+    ((sum += i))
+done
+echo "The sum is: $sum"
+
+
+sum=0
+for ((i=1; i<=100; ))
+do
+    ((sum += i))
+    ((i++))
+done
+echo "The sum is: $sum"
+
+
+
+sum=0
+i=0
+for (( ; ; ))
+do
+     if(( i>100 )); then
+        break
+    fi
+    ((sum += i))
+    ((i++))
+done
+echo "The sum is: $sum"
+
 
